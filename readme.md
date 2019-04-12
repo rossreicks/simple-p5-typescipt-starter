5422cb3310a7436a171955e231b90a54624a834e
Ross Reicks <rreicks13@gmail.com>
2019-04-11 19:46:32 -0500

    init project

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Simple p5 Typescript starter
+
+This is a fork from Gaweph/p5-typescript-starter
+
+bundled using parcel bundler for a no configuration needed project
+
+## To Start
+```git clone``` 
+```npm install```
+```npm start```
+Open browser to localhost:1234
---
+++ sketch/sketch.ts
@@ -0,0 +1,26 @@
+import { Morph }  from './morph';
+import p5 = require('p5');
+var sketch = (p: p5) => {
+
+    const morph = new Morph();
+
+    p.preload = () => {
+
+    }
+    
+    p.setup = () => {
+        p.createCanvas(p.windowWidth, p.windowHeight);
+        morph.setup(p);
+    }
+
+    p.windowResized = () => {
+        p.resizeCanvas(p.windowWidth, p.windowHeight);
+    }
+
+    p.draw = () => {
+        p.background(100);
+        morph.draw(p);
+    }
+}
+
+var sketchP = new p5(sketch);
